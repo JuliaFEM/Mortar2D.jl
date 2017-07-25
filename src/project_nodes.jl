56ac8cd18315aa563be5af28dad3afc843f114bc
Jukka Aho <ahojukka5@gmail.com>
2017-07-25 21:32:27 +0300

    Add function to calculate contact segmentation (#4)
    Also fix finding projection from master to slave analytically.
    There is a special case when normal field on slave is constant.
    In that case we are no longer solving quadratic equation but
    linear one.

--- src/project_nodes.jl
+++ src/project_nodes.jl
@@ -24,6 +24,14 @@ Find the projection of a master node `xm`, to the slave surface with nodes
 """
 function project_from_master_to_slave{T<:Number}(::Type{Val{:Seg2}}, xm::Vector{T}, xs1::Vector{T}, xs2::Vector{T}, ns1::Vector{T}, ns2::Vector{T})
 
+    # Special case when normal is constant. Then we have
+    # unique solution and linear equation to solve.
+    if isapprox(ns1, ns2)
+        nom = -2*ns1[1]*xm[2] + ns1[1]*xs1[2] + ns1[1]*xs2[2] + 2*ns1[2]*xm[1] - ns1[2]*xs1[1] - ns1[2]*xs2[1]
+        denom = ns1[1]*xs1[2] - ns1[1]*xs2[2] - ns1[2]*xs1[1] + ns1[2]*xs2[1]
+        return nom/denom
+    end
+
     nom_b = 2*ns1[1]*xm[2] - 2*ns1[1]*xs1[2] - 2*ns1[2]*xm[1] + 2*ns1[2]*xs1[1] - 2*ns2[1]*xm[2] + 2*ns2[1]*xs2[2] + 2*ns2[2]*xm[1] - 2*ns2[2]*xs2[1]
     denom_b = ns1[1]*xs1[2] - ns1[1]*xs2[2] - ns1[2]*xs1[1] + ns1[2]*xs2[1] - ns2[1]*xs1[2] + ns2[1]*xs2[2] + ns2[2]*xs1[1] - ns2[2]*xs2[1]
     nom_c = -2*ns1[1]*xm[2] + ns1[1]*xs1[2] + ns1[1]*xs2[2] + 2*ns1[2]*xm[1] - ns1[2]*xs1[1] - ns1[2]*xs2[1] - 2*ns2[1]*xm[2] + ns2[1]*xs1[2] + ns2[1]*xs2[2] + 2*ns2[2]*xm[1] - ns2[2]*xs1[1] - ns2[2]*xs2[1]
@@ -34,6 +42,5 @@ function project_from_master_to_slave{T<:Number}(::Type{Val{:Seg2}}, xm::Vector{
     c = nom_c / denom_c
     d = b^2 - 4*a*c
     sols = [-b + sqrt(d), -b - sqrt(d)]/(2.0*a)
-    info(sols)
     return sols[indmin(abs.(sols))]
 end

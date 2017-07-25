426b5f5cb54408c4f025ffbb0849409423c83085
Jukka Aho <ahojukka5@gmail.com>
2017-07-25 18:09:42 +0300

    add projection code

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ const to = TimerOutput()
 
 test_files = String[]
 push!(test_files, "test_calculate_normals.jl")
+push!(test_files, "test_project_nodes.jl")
 
 @testset "Mortar2D.jl" begin
     for fn in test_files

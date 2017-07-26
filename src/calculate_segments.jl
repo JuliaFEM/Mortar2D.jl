2fb170461a4f3770fa977132299772dccfd13610
Jukka Aho <ahojukka5@gmail.com>
2017-07-26 22:36:51 +0300

    improve calling syntax

--- src/calculate_segments.jl
+++ src/calculate_segments.jl
@@ -10,29 +10,26 @@ Returns a dictionary, where key is slave element id and values is a list of
 master elements giving contribution to that slave elements and xi-coordinates
 of slave side element.
 """
-function calculate_segments(slave_elements, master_elements, element_types, coords, normals)
+function calculate_segments(slave_ids, master_ids, elements, element_types, coords, normals)
     S = Dict()
-    for (sid, scon) in slave_elements
+    for sid in slave_ids
         @assert element_types[sid] == :Seg2
+        haskey(S, sid) || (S[sid] = [])
+        scon = elements[sid]
         xs1 = coords[scon[1]]
         xs2 = coords[scon[2]]
         ns1 = normals[scon[1]]
         ns2 = normals[scon[2]]
-        for (mid, mcon) in master_elements
+        for mid in master_ids
             @assert element_types[mid] == :Seg2
+            mcon = elements[mid]
             xm1 = coords[mcon[1]]
             xm2 = coords[mcon[2]]
             xi1a = project_from_master_to_slave(Val{:Seg2}, xm1, xs1, xs2, ns1, ns2)
             xi1b = project_from_master_to_slave(Val{:Seg2}, xm2, xs1, xs2, ns1, ns2)
             xi1 = clamp.([xi1a; xi1b], -1.0, 1.0)
             l = 1/2*abs(xi1[2]-xi1[1])
-            if isapprox(l, 0.0)
-                # no contribution from master so slave, we are done
-                continue
-            end
-            if !haskey(S, sid)
-                S[sid] = []
-            end
+            isapprox(l, 0.0) && continue
             push!(S[sid], (mid, xi1))
         end
     end

8d55fa5fe40b0492115cb32b44f37f82a5cf497b
Jukka Aho <ahojukka5@gmail.com>
2019-10-10 18:39:48 +0300

    Make package compatible with Julia 1.0

--- test/test_calculate_mortar_assembly.jl
+++ test/test_calculate_mortar_assembly.jl
@@ -1,17 +1,17 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/Mortar2D.jl/blob/master/LICENSE
 
-using Base.Test
+using Test
 using Mortar2D: calculate_mortar_assembly
 
 @testset "calculate mortar assembly" begin
     ns = 10 # number of slaves in interface
     nm = 9 # number of masters in interface
 
-    Xs = Dict(i => [x, 1/2] for (i, x) in enumerate(linspace(0, 1, ns)))
+    Xs = Dict(i => [x, 1/2] for (i, x) in enumerate(range(0.0, stop=1.0, length=ns)))
     Es = Dict(i => [i, i+1] for i=1:(ns-1))
-    Xm = Dict(ns+i => [x, 1/2] for (i, x) in enumerate(linspace(0, 1, nm)))
-    Em = Dict(i => [i, i+1]+1 for i=ns:ns+nm-2)
+    Xm = Dict(ns+i => [x, 1/2] for (i, x) in enumerate(range(0.0, stop=1.0, length=nm)))
+    Em = Dict(i => [i+1, i+2] for i=ns:ns+nm-2)
     element_types = Dict(i => :Seg2 for i=1:(ns+nm))
 
     slave_element_ids = collect(keys(Es))
@@ -32,8 +32,8 @@ using Mortar2D: calculate_mortar_assembly
     @test isapprox(us, us_expected)
 
     # test case 2, linear load
-    um = linspace(0, 1, nm)
+    um = range(0.0, stop=1.0, length=nm)
     us = D[s,s] \ (M[s,m]*um)
-    us_expected = linspace(0, 1, ns)
+    us_expected = range(0.0, stop=1.0, length=ns)
     @test isapprox(us, us_expected)
 end
